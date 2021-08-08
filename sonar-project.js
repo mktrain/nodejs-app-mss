55346fbf3a0ff71ce43e339ef865f5c46f53a395
root <root@jenkins-server.asia-south1-a.c.vibrant-petal-317106.internal>
2021-08-08 14:12:13 +0000

    Added sonar url

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http:///',
+  serverUrl: '/http://34.131.71.42:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
