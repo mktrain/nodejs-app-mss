9f1b60e16294920eb145eeb4b492572dcea5a864
mktrain <80453983+mktrain@users.noreply.github.com>
2021-08-16 10:07:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ res.send("<h2>Welcome to Node JS express app</h2>"+appEnv.url+appEnv.port+port+p
 }).listen(9009);
 console.log(__dirname+"/images/mithunlogo.jpg");
 */
-app.get('/mithuntechnologies', function(request, response) {
+app.get('/POC', function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app</h2>");
-    response.write("<h2><center><u>Node JS  Application </u></center></h2>");
+    response.write("Welcome to DevOps POC");
 	
-    response.write("<h2><center>Welcome to  DevOps POC <center></h2>" );
+    response.write("This is NodeJs Application" );
     response.end();
     
   })
