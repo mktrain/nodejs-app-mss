47a7cf17fc70e1417d8ad2e00d83f89ce18fc2db
mktrain <80453983+mktrain@users.noreply.github.com>
2021-11-15 12:59:44 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://34.131.120.11:9000/',
+  serverUrl: 'http://34.93.111.157:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJs',
-	    'sonar.login': 'a5fde037465b2c59a0f39d1811eb4efc804d248e',
+	    'sonar.login': 'eaed64d74c779f0727c752e034dfca22c2f7af62',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
